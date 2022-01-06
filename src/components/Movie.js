e2c90667ab28391d327d5173609129171fd4c0bd
chochozabe <cintamani_whyk@naver.com>
2022-01-07 00:01:48 +0900

    Mod : 상세화면 데이터 처리

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,21 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie(props) {
+  const { id, coverImg, title, summary, genres } = props;
+
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>
-        <Link to="/movie">{title}</Link>
-      </h2>
+      <h2>{id ? <Link to={`movie/${id}`}>{title}</Link> : ""}</h2>
       <p>{summary}</p>
-      <ul>{genres && genres.map((g, index) => <li key={g}>{g}</li>)}</ul>
+      <ul>{genres && genres.map((g) => <li key={g}>{g}</li>)}</ul>
     </div>
   );
 }
 
 Movie.propTypes = {
+  id: PropTypes.number,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
